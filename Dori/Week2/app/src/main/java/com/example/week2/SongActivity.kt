9ada4ccb528922912e61cf33bf883f16b66754ba
Administrator <kdy99ky@gmail.com>
2025-09-28 23:41:46 +0900

    feat : 미니플레이바 앨범을 누르면은 SongActivity.kt로 넘어가고 btnBack을 누르면은 다시 돌아감

--- Dori/Week2/app/src/main/java/com/example/week2/SongActivity.kt
+++ Dori/Week2/app/src/main/java/com/example/week2/SongActivity.kt
@@ -1,15 +1,25 @@
 package com.example.week2
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.week2.databinding.ActivityMainBinding
+import com.example.week2.databinding.ActivitySongBinding
 
 class SongActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivitySongBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySongBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
