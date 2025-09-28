ff643063998821f2421cd5f7ad8f900bfd1087ce
Administrator <kdy99ky@gmail.com>
2025-09-28 22:45:50 +0900

    feat : 하단 네비게이션 바 와 미니플레이어 바 구현

---
+++ Dori/Week2/app/src/main/java/com/example/week2/SongActivity.kt
@@ -0,0 +1,15 @@
+package com.example.week2
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.week2.databinding.ActivityMainBinding
+
+class SongActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
