20d475de5099075dc298efb7ca83c11b91ebd53c
Administrator <kdy99ky@gmail.com>
2025-09-29 00:09:16 +0900

    파일 복구

---
+++ Dori/Week2/app/src/main/java/com/example/week2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.week2
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.week2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.miniPlayer.mainPlayer.setOnClickListener {
+            startActivity(Intent(this, SongActivity::class.java))
+        }
+
+    }
+}
