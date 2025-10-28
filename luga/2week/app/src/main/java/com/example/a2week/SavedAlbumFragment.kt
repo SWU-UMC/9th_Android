60f701b799b4e066f67d51f8390acd1a02cd75b2
ochyeon <136903107+ochyeon@users.noreply.github.com>
2025-10-29 02:02:52 +0900

    feat: '저장된 앨범' RecyclerView & 버튼 이미지 전환 구현

--- luga/2week/app/src/main/java/com/example/a2week/SavedAlbumFragment.kt
+++ luga/2week/app/src/main/java/com/example/a2week/SavedAlbumFragment.kt
@@ -5,19 +5,63 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a2week.databinding.FragmentLockerSavedalbumBinding
 
 class SavedAlbumFragment: Fragment() {
 
-    lateinit var binding: FragmentLockerSavedalbumBinding
+    private var _binding: FragmentLockerSavedalbumBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLockerSavedalbumBinding.inflate(inflater, container, false)
+        _binding = FragmentLockerSavedalbumBinding.inflate(inflater, container, false)
+
+        // 샘플 data
+        val savedSongs = mutableListOf(
+            AlbumData(R.drawable.img_album_exp, "앨범 이름", "가수 이름"),
+            AlbumData(R.drawable.img_album_exp, "앨범 이름", "가수 이름"),
+            AlbumData(R.drawable.img_album_exp2, "앨범 이름", "가수 이름"),
+            AlbumData(R.drawable.img_album_exp2, "앨범 이름", "가수 이름"),
+            AlbumData(R.drawable.img_album_exp, "앨범 이름", "가수 이름"),
+            AlbumData(R.drawable.img_album_exp, "앨범 이름", "가수 이름"),
+            AlbumData(R.drawable.img_album_exp2, "앨범 이름", "가수 이름"),
+            AlbumData(R.drawable.img_album_exp2, "앨범 이름", "가수 이름"),
+            AlbumData(R.drawable.img_album_exp, "앨범 이름", "가수 이름"),
+            AlbumData(R.drawable.img_album_exp, "앨범 이름", "가수 이름"),
+            AlbumData(R.drawable.img_album_exp2, "앨범 이름", "가수 이름"),
+            AlbumData(R.drawable.img_album_exp2, "앨범 이름", "가수 이름"))
+
+        val adapter = SavedSongAdapter(savedSongs) { clickedSong ->
+            val bundle = Bundle().apply {
+                putInt("albumImg", clickedSong.img)
+                putString("albumTitle", clickedSong.title)
+                putString("albumSinger", clickedSong.singer)
+            }
+
+            val albumFragment = AlbumFragment().apply {
+                arguments = bundle
+            }
+
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, albumFragment)
+                .addToBackStack(null)
+                .commitAllowingStateLoss()
+        }
+
+        binding.savedAlbumListRv.apply{
+            layoutManager = LinearLayoutManager(requireContext())
+            this.adapter = adapter
+        }
 
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
